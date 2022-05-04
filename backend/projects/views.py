2cf6c7eb99255a8e0c4ba986a459c4e684023d7d
codi kingsbury <codomodo11@yahoo.com>
2022-05-04 09:13:21 -0700

    Made backend GET and POST to retrieve all projects

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,11 +1,24 @@
+from logging import raiseExceptions
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Projects
+from .serializers import ProjectsSerializer
 
-@api_view (['GET'])
+@api_view (['GET','POST'])
 def projects_list(request):
+    if request.method == 'GET':
+        projects = Projects.objects.all()
+        serializer = ProjectsSerializer(projects, many=True)
+        return Response(serializer.data)
 
+    elif request.method == 'POST':
+        serializer = ProjectsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    return Response ('ok')
